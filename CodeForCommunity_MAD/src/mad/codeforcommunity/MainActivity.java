14d608bb7ea281410df0ab58307be8bb87270c48
dylanzingler <dylanzingler@gmail.com>
2013-02-16 17:29:12 +0000

    Got the start project button to link to the other activity.

--- CodeForCommunity_MAD/src/mad/codeforcommunity/MainActivity.java
+++ CodeForCommunity_MAD/src/mad/codeforcommunity/MainActivity.java
@@ -1,5 +1,8 @@
 package mad.codeforcommunity;
 
+
+
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -13,28 +16,12 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		Button findProject = (Button) findViewById(R.id.bFind);
+		
 		Button startProject = (Button) findViewById(R.id.bStart_Project);
-
-
-		findProject.setOnClickListener(new View.OnClickListener() {
-
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-
-			}
-		});
-
-
+		
 		startProject.setOnClickListener(new View.OnClickListener() {
-
-			public void onClick(View v) {
+			
+			public void onClick(View v) {	
 				try {
 					Class startClass = Class.forName("mad.codeforcommunity.Start_Project");
 					Intent projectStart = new Intent(MainActivity.this, startClass);
@@ -43,9 +30,17 @@ public class MainActivity extends Activity {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-
+				
+				
 			}
 		});
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
 

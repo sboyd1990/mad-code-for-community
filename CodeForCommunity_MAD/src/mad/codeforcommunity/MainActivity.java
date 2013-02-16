392817bfda888a03f74ee45c6ffb3edcbcc63ffe
dylanzingler <dylanzingler@gmail.com>
2013-02-16 16:55:47 +0000

    trying to get the start project button open the Start_Project activity, having trouble. Not sure if its the menu method in the MainActivity or not.

--- CodeForCommunity_MAD/src/mad/codeforcommunity/MainActivity.java
+++ CodeForCommunity_MAD/src/mad/codeforcommunity/MainActivity.java
@@ -2,7 +2,10 @@ package mad.codeforcommunity;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -16,6 +19,33 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
+		Button findProject = (Button) findViewById(R.id.bFind);
+		Button startProject = (Button) findViewById(R.id.bStart_Project);
+
+
+		findProject.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+
+		startProject.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				try {
+					Class startClass = Class.forName("mad.codeforcommunity.Start_Project");
+					Intent projectStart = new Intent(MainActivity.this, startClass);
+					startActivity(projectStart);
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		});
 		return true;
 	}
 

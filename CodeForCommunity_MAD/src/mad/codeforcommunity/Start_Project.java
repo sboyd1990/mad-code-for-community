710b1f86b619188ade3aa4237ebf7a74c55ae95a
vvendemia1 <vvendemia1@gmail.com>
2013-02-23 22:40:53 +0000

    Added AWS Functionality from the Demo, but it is not set up to work with our project yet.  CAUTION:  Do not run the code until you talk with me first, you will need to configure your eclipse.ini file to allow for a larger heap size.  With that being said, take a look at the code.  It seems pretty simple, basically we will create buckets of information, these will be our volunteer events.  These events will be stored in our location of Amazon S3 server, and pulled when needed.

--- CodeForCommunity_MAD/src/mad/codeforcommunity/Start_Project.java
+++ CodeForCommunity_MAD/src/mad/codeforcommunity/Start_Project.java
@@ -1,12 +1,69 @@
 package mad.codeforcommunity;
 
+
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 
 public class Start_Project extends Activity {
 
+	private static final String success = "Welcome to The AWS Browser Demo!";
+	private static final String fail = "Load Failed. Please Try Restarting the Application.";
+	
+
+	protected Button s3Button;
+	protected TextView welcomeText;
+	
+    public static AmazonClientManager clientManager = null;
+    	
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        s3Button = (Button) findViewById(R.id.main_storage_button);                           
+
+        clientManager = new AmazonClientManager();
+
+     	if ( Start_Project.clientManager.hasCredentials() ){
+    		s3Button.setVisibility(View.VISIBLE);
+    		this.wireButtons();
+    	} 
+        else {
+    		this.displayCredentialsIssueAndExit();
+    		welcomeText.setText(fail);
+    	}       
+    }
+        
+    private void wireButtons(){
+       s3Button.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+    			startActivity(new Intent(Start_Project.this, S3Menu.class));
+			}
+		});
+    }
+        
+    protected void displayCredentialsIssueAndExit() {
+        AlertDialog.Builder confirm = new AlertDialog.Builder( this );
+        confirm.setTitle("Credential Problem!");
+        confirm.setMessage( "AWS Credentials not configured correctly.  Please review the README file." );
+        confirm.setNegativeButton( "OK", new DialogInterface.OnClickListener() {
+            public void onClick( DialogInterface dialog, int which ) {
+            	Start_Project.this.finish();
+            }
+        } );
+        confirm.show().show();                
+    }
+
+	
+/*	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -20,5 +77,5 @@ public class Start_Project extends Activity {
 		
 		Button submit = (Button) findViewById(R.id.bSubmit_Project);
 	}
-
+	*/
 }

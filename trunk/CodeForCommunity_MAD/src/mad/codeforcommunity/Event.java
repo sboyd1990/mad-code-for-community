79a9f87d68fc770364b160fefb44dcfab023fcdc
elysham13@gmail.com <elysham13@gmail.com@aaac2490-4492-17b7-c1f4-9a22e3e695d3>
2013-03-20 06:16:49 +0000

    Updated Event object

--- trunk/CodeForCommunity_MAD/src/mad/codeforcommunity/Event.java
+++ trunk/CodeForCommunity_MAD/src/mad/codeforcommunity/Event.java
@@ -3,35 +3,113 @@ package mad.codeforcommunity;
 import java.util.ArrayList;
 
 public class Event {
-	
-	String host;
-	String name;
-	String description;
-	String address;
-	String contact;
-	
-	
-	public Event(String h, String n, String d, String a, String c) {
-		this.host = h;
-		this.name = n;
-		this.description = d;
-		this.address = a;
-		this.contact = c;
+	// title
+	// date
+	// description
+	// location
+	// contact_name
+	// contact_email
+	// contact_phone
+
+	private String title;
+	private String date;
+	private String description;
+	private String location;
+	private String contactName;
+	private String contactEmail;
+	private String contactPhone;
+
+
+	/**
+	 * @param title
+	 * @param date
+	 * @param description
+	 * @param location
+	 * @param contactName
+	 * @param contactEmail
+	 * @param contactPhone
+	 */
+	public Event(String title, String date, String description,
+			String location, String contactName, String contactEmail,
+			String contactPhone) {
+		this.title = title;
+		this.date = date;
+		this.description = description;
+		this.location = location;
+		this.contactName = contactName;
+		this.contactEmail = contactEmail;
+		this.contactPhone = contactPhone;
 	}
-	
+
+
+	//For testing only!
+	/**
+	 * @param t
+	 * @param d
+	 */
+	public Event(String t, String d) {
+		this.title= t;
+		this.date= d;
+		
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public String getDate() {
+		return date;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public String getLocation() {
+		return location;
+	}
+
+
+	public String getContactName() {
+		return contactName;
+	}
+
+
+	public String getContactEmail() {
+		return contactEmail;
+	}
+
+
+	public String getContactPhone() {
+		return contactPhone;
+	}
+
+
+
+
+
 	
 	public static ArrayList<String> listOfEventInfo(Event e) {
 		ArrayList<String> info = new ArrayList<String>();
-		
-		info.add(e.host);
-		info.add(e.name);
+	
+		info.add(e.title);
+		info.add(e. date);
 		info.add(e.description);
-		info.add(e.address);
-		info.add(e.contact);
+		info.add(e.location);
+		info.add(e.contactName);
+		info.add(e.contactEmail);
+		info.add(e.contactPhone);
 		
 		return info;
 	}
-	
+
+
+
 	
 	
 	
